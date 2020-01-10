78b1235b8a8e53ce7c2ff1a8c6a77674b74d1a8a
Sharda Singh <shardaprasad001@gmail.com>
2020-01-10 16:41:49 +0530

    Junit Cucumber multiple use cases covered into this project

---
+++ src/main/java/sy/junitcucumber/App.java
@@ -0,0 +1,13 @@
+package sy.junitcucumber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/sy/junitcucumber/UserTest.java
@@ -0,0 +1,8 @@
+package sy.junitcucumber;
+
+import org.junit.runner.RunWith;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+public class UserTest {
+}
